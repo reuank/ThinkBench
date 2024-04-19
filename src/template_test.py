b5d2d12bf336f576194a8c297976b5bacc18b693
Leon Knauer <git@leonknauer.com>
2024-04-19 14:51:29 +0200

    Expanded template test tool

--- src/template_test.py
+++ src/template_test.py
@@ -1,11 +1,12 @@
 from typing import List, Dict
 
-import jinja2
+from transformers import AutoTokenizer
 from jinja2 import Template, FileSystemLoader, Environment
 
 from benchmark import NonCoTStandardBenchmark, CoTStandardBenchmark, Benchmark
 from dataset import SingleDataInstance
 from inference import MessageHistory
+from model import ModelConfig, HFModelConfig
 from prompt import PromptTemplateStep, PromptCompletionStep, PromptTextStep
 
 
@@ -39,7 +40,7 @@ def convert_chain_to_messages(prompt_chains, single_data_instance) -> List[Dict]
     return message_history.messages
 
 
-def test_template_on_benchmark(template_name: str, benchmark: Benchmark, add_system_message: bool = True):
+def test_template_on_benchmark(model_config: HFModelConfig, benchmark: Benchmark, add_system_message: bool = True):
     system_message = {
         "role": "system",
         "content": "You are a friendly chatbot who always responds in the style of a pirate",
@@ -53,29 +54,59 @@ def test_template_on_benchmark(template_name: str, benchmark: Benchmark, add_sys
     if add_system_message:
         generated_messages.insert(0, system_message)
 
-    chat_template = load_template(template_name)
-    prompt = chat_template.render(messages=generated_messages, bos_token="<s>", eos_token="</s>", add_generation_prompt=True)
+    chat_template = model_config.chat_template
+    my_prompt = chat_template.render(messages=generated_messages, bos_token="<s>", eos_token="</s>", add_generation_prompt=True)
 
-    return prompt
+    hf_tokenizer = AutoTokenizer.from_pretrained(model_config.hf_tokenizer, use_fast=False)
+    hf_tokenized_chat = hf_tokenizer.apply_chat_template(generated_messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+
+    return my_prompt, hf_tokenizer.decode(hf_tokenized_chat[0])
 
 
-def test_all_templates_with_all_benchmarks():
-    template_names = [
-        "llama-2-chat",
-        "orca-2"
-    ]
+def get_hf_tokenizer_reference(hf_model_path: str, messages: []):
+    tokenizer = AutoTokenizer.from_pretrained(hf_model_path)
+    tokenized_chat = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
 
+    return tokenizer.decode(tokenized_chat[0])
+
+
+def test_all_templates_with_many_benchmarks(benchmarks):
+    for model_name in ModelConfig.get_all_names():
+        model_config: HFModelConfig = ModelConfig.get_by_name(model_name)
+
+        for benchmark in benchmarks:
+            print("="*50)
+            print("="*50)
+            print(f"Testing Template {model_config.chat_template_name} on {benchmark.__class__.__name__}:")
+            print("=" * 50)
+            my_output, hf_reference = test_template_on_benchmark(model_config, benchmark)
+            print(my_output)
+            print("=" * 50)
+            print(hf_reference)
+
+
+def compare_my_and_hf(model_name: str):
+    print("=" * 50)
+    print("My Output:")
+    print("=" * 50)
+    my_output, hf_reference = test_template_on_benchmark(ModelConfig.get_by_name(model_name),
+                                                         CoTStandardBenchmark())
+    print(my_output)
+    print("=" * 50)
+    print("HF Reference:")
+    print("=" * 50)
+    print(hf_reference)
+    print("=" * 50)
+    print(f"Do they match? {my_output == hf_reference}")
+
+if __name__ == '__main__':
     benchmarks = [
         NonCoTStandardBenchmark(),
         CoTStandardBenchmark()
     ]
 
-    for template_name in template_names:
-        for benchmark in benchmarks:
-            print("="*50)
-            print(f"Testing Template {template_name} on {benchmark.__class__.__name__}:")
-            print("=" * 50)
-            print(test_template_on_benchmark(template_name, benchmark))
+    # get_hf_tokenizer_reference("meta-llama/Llama-2-7b-chat-hf", )
+    # test_all_templates_with_many_benchmarks(benchmarks)
 
-if __name__ == '__main__':
-    test_all_templates_with_all_benchmarks()
+    compare_my_and_hf("phi-2")
+    #compare_my_and_hf("llama-2-7b-chat")
