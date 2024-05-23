f9e39984a1ad19bbff46630f32786af412de40dc
Leon Knauer <git@leonknauer.com>
2024-05-24 01:02:08 +0200

    Added information for random runs to the total result

--- thinkbench/testcase.py
+++ thinkbench/testcase.py
@@ -11,6 +11,9 @@ class TestCaseResult(TypedDict):
     dataset_name: str
     benchmark_name: str
     label_numbering: str
+    label_permutation: str
+    n_random: int
+    random_seed: int
     hostname: str
     inference_backend: str
     inference_backend_properties: Optional[Dict]
@@ -28,25 +31,27 @@ class TestCase:
     dataset: Dataset
     benchmark: Benchmark
     limit: int
-    random: int
+    n_random: int
+    random_seed: int
     label_numbering: Numbering
     label_permutation: Permutation
     use_chat_template: bool
 
-    def __init__(self, dataset: Dataset, limit: int, random: int, label_numbering: Numbering = Numbering.get_default(), label_permutation: Permutation = Permutation.get_default(), benchmark: Benchmark = Benchmark.get_default(), use_chat_template: bool = True):
+    def __init__(self, dataset: Dataset, limit: int, n_random: int, label_numbering: Numbering = Numbering.get_default(), label_permutation: Permutation = Permutation.get_default(), benchmark: Benchmark = Benchmark.get_default(), use_chat_template: bool = True):
         self.dataset = dataset
         self.limit = min(limit, len(dataset.test_split)) if limit != -1 else len(dataset.test_split)
-        self.random = random if limit == -1 else -1
+        self.n_random = n_random if limit == -1 else -1
+        self.random_seed = int() if n_random != -1 else -1
         self.label_numbering = label_numbering
         self.label_permutation = label_permutation
         self.benchmark = benchmark
         self.use_chat_template = use_chat_template
 
     def prepare_test_dataset(self):
-        if self.random != -1:
-            random.seed(1337)
+        if self.n_random != -1:
+            random.seed(self.random_seed)
             instance_ids = []
-            while len(instance_ids) < self.random:
+            while len(instance_ids) < self.n_random:
                 instance_ids.append(random.randint(0, len(self.dataset.test_split)))
 
             return [self.dataset.get_single_test_instance(instance_id)
@@ -67,8 +72,10 @@ class TestCase:
         test_case_info = f"""=========== Running New Test Case ===========
 Dataset: {self.dataset.name}
 Limit: {self.limit}
-Random Samples: {self.random}
+Random Samples: {self.n_random if self.n_random != -1 else 'None'}
+Random Seed: {self.random_seed if self.n_random != -1 else 'None'}
 Label Numbering: {self.label_numbering}
+Label Permutation: {self.label_permutation}
 Benchmark: {self.benchmark.name}  
 Use Chat Template: {self.use_chat_template}
 ============================================="""
